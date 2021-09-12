3f8a4471fc703390986d6c49be3ce9e78b1d2ef4
marcos <marcos.correa@docato.com.br>
2021-09-12 15:40:10 -0300

    por env

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000
+  console.log('PORT -> ',process.env.PORT)
+  await app.listen(port);
 }
 bootstrap();
